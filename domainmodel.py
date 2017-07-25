03ec9e328c88696fab97abd465d2816a74520b64
Kef <upload234@mail.ru>
2017-07-25 15:57:32 +0300

    яничегонепонял.жпг
    Учи гит, дерево

--- domainmodel.py
+++ domainmodel.py
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 from billitem import BillItem
 from PyQt5.QtCore import QObject
 
@@ -39,42 +38,4 @@ class DomainModel(QObject):
 
     def testMethod(self):
         print("domain model test method call")
-=======
-from billitem import BillItem
-from PyQt5.QtCore import QObject
 
-
-class DomainModel(QObject):
-
-    dict_list = ["category", "period", "priority", "project", "shipment", "status", "vendor"]
-
-    def __init__(self, parent=None, persistenceFacade=None):
-        super(DomainModel, self).__init__(parent)
-
-        self._facade = persistenceFacade
-        self._data = list()
-        self._dicts = dict()
-
-    def initModel(self):
-        print("init domain model")
-
-        # choose item builder depending on data source
-        if self._facade.engineType == "csv":
-            builder = BillItem.fromRawList
-
-        if self._facade.engineType == "sqlite":
-            builder = BillItem.fromSqliteTuple
-
-        self._dicts = self._facade.fetchDicts(self.dict_list)
-
-        self._data = [builder(d) for d in self._facade.fetchAllData()]
-
-    def rowCount(self):
-        return len(self._data)
-
-    def getItemAtRow(self, row: int):
-        return self._data[row]
-
-    def getDicts(self):
-        return self._dicts
->>>>>>> Stashed changes
--- sqliteengine.py
+++ sqliteengine.py
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 import sqlite3
 from PyQt5.QtCore import QObject
 
@@ -51,59 +50,3 @@ class SqliteEngine(QObject):
 
     def shutdownEngine(self):
         self._connection.close()
-=======
-import sqlite3
-from PyQt5.QtCore import QObject
-
-
-class SqliteEngine(QObject):
-    def __init__(self, parent=None):
-        super(SqliteEngine, self).__init__(parent)
-
-        # TODO make properties
-        self.engineType = "sqlite"
-        self._inFileName = None
-        self._connection = None
-
-    def initEngine(self, fileName=None):
-        print("init SQLite engine")
-        self._inFileName = fileName
-        self._connection = sqlite3.connect(self._inFileName)
-
-    def fetchAllData(self):
-        print("fetch all records")
-        with self._connection:
-            cursor = self._connection.execute("SELECT main.bill.bill_id AS id"
-                                              ", main.bill.bill_date"
-                                              ", main.bill.bill_name"
-                                              ", main.bill.bill_category"
-                                              ", main.bill.bill_vendor"
-                                              ", main.bill.bill_cost"
-                                              ", main.bill.bill_project"
-                                              ", main.bill.bill_desc"
-                                              ", main.bill.bill_shipment_time"
-                                              ", main.bill.bill_status"
-                                              ", main.bill.bill_priority"
-                                              ", main.bill.bill_shipment_date"
-                                              ", main.bill.bill_shipment_status"
-                                              ", main.bill.bill_week"
-                                              ", main.bill.bill_note"
-                                              "  FROM bill "
-                                              " WHERE bill.bill_id > 0")
-            return cursor.fetchall()
-
-    def fetchDicts(self, dict_list: list):
-        print("fetching dicts")
-
-        def fetchDict(connection, dict_name: str):
-            cursor = connection.execute("SELECT " + dict_name + "_id, " + dict_name + "_name"
-                                        "  FROM " + dict_name + " ")
-            return cursor.fetchall()
-
-        return [fetchDict(self._connection, d) for d in dict_list]
-        # for d in dict_list:
-        #     print(fetchDict(self._connection, d))
-
-    def shutdownEngine(self):
-        self._connection.close()
->>>>>>> Stashed changes
